362dd941250d6abf149cfe50947eb84fd17be4b1
giannis <giannis.katsanos@gmail.com>
2012-03-22 17:57:57 +0200

    First layout for the sharing functionality

--- src/app/gui/AppFrame.java
+++ src/app/gui/AppFrame.java
@@ -1,51 +1,37 @@
 package app.gui;
 
 import javax.swing.GroupLayout;
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JMenuBar;
-import javax.swing.JPanel;
-import javax.swing.LayoutStyle;
+import javax.swing.JScrollPane;
 import javax.swing.WindowConstants;
 
-import com.google.common.eventbus.Subscribe;
-
-import app.gui.components.buttons.GoButton;
 import app.gui.components.menu.MenuBar;
-import app.gui.components.panels.ImagesPanel;
-import app.gui.components.panels.ManipulationPanel;
-import app.gui.components.panels.OutputPanel;
-import app.gui.components.panels.SharePanel;
+import app.gui.components.scrolling.AppPanelScrollPane;
 import app.gui.events.ClearFieldsEvent;
 import app.gui.events.EventBusService;
 import app.gui.events.ImagesCompressedEvent;
+
+import com.google.common.eventbus.Subscribe;
 
 public class AppFrame extends JFrame {
 
 	private static final long serialVersionUID = -7180367288739459084L;
 	
 	private JMenuBar menuBar;
-	private JPanel imagesPanel;
-	private JPanel manipulationPanel;
-	private JPanel sharePanel;
-	private JPanel outputPanel;
-	private JButton goButton;
+	private JScrollPane appPanelScrollPane;
 	
 	public AppFrame() {
 		GroupLayout appFrameLayout 
-			= new GroupLayout((JComponent)getContentPane());
+			= new GroupLayout((JComponent) getContentPane());
 		getContentPane().setLayout(appFrameLayout);
-		setSize(800, 600);
+		setSize(810, 610);
 		setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
 		
 		menuBar = new MenuBar();
 		setJMenuBar(menuBar);
-		imagesPanel = new ImagesPanel();
-		manipulationPanel = new ManipulationPanel();
-		sharePanel = new SharePanel();
-		outputPanel = new OutputPanel();
-		goButton = new GoButton();
+		appPanelScrollPane = new AppPanelScrollPane();
 
 		setContent(appFrameLayout);
 		pack();
@@ -59,55 +45,15 @@ public class AppFrame extends JFrame {
 	}
 	
 	private void setContent(GroupLayout appFrameLayout) {
-		appFrameLayout.setVerticalGroup(appFrameLayout.createSequentialGroup()
-			.addContainerGap()
-			.addComponent(imagesPanel, GroupLayout.PREFERRED_SIZE, 133, 
-					GroupLayout.PREFERRED_SIZE)
-			.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-			.addComponent(manipulationPanel, GroupLayout.PREFERRED_SIZE, 133, 
-					GroupLayout.PREFERRED_SIZE)
-			.addGap(17)
-			.addComponent(sharePanel, GroupLayout.PREFERRED_SIZE, 111, 
-					GroupLayout.PREFERRED_SIZE)
-			.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-			.addGroup(
-				appFrameLayout.createParallelGroup()
-			    .addComponent(outputPanel, GroupLayout.Alignment.LEADING, 0, 
-			    		88, Short.MAX_VALUE)
-			    .addGroup(
-			    	GroupLayout.Alignment.LEADING, 
-			    	appFrameLayout.createSequentialGroup()
-			        .addGap(0, 26, Short.MAX_VALUE)
-			        .addComponent(goButton, GroupLayout.PREFERRED_SIZE, 34, 
-			        	GroupLayout.PREFERRED_SIZE)
-			        .addGap(28)
-			    )
-			)
-			.addContainerGap()
+		appFrameLayout.setHorizontalGroup(
+			appFrameLayout.createSequentialGroup()
+			.addComponent(appPanelScrollPane, GroupLayout.PREFERRED_SIZE, 800, 
+				GroupLayout.PREFERRED_SIZE)
 		);
-		
-		appFrameLayout.setHorizontalGroup(appFrameLayout.createSequentialGroup()
-			.addContainerGap()
-			.addGroup(
-				appFrameLayout.createParallelGroup()
-			    .addGroup(
-			    	GroupLayout.Alignment.LEADING, 
-			    	appFrameLayout.createSequentialGroup()
-			        .addComponent(outputPanel, GroupLayout.PREFERRED_SIZE, 642, 
-			        	GroupLayout.PREFERRED_SIZE)
-			        .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-			        .addComponent(goButton, GroupLayout.PREFERRED_SIZE, 103, 
-			        	GroupLayout.PREFERRED_SIZE)
-			        .addGap(0, 11, Short.MAX_VALUE)
-			    )
-			    .addComponent(imagesPanel, GroupLayout.Alignment.LEADING, 0, 
-			    	768, Short.MAX_VALUE)
-			    .addComponent(manipulationPanel, GroupLayout.Alignment.LEADING, 
-			    	0, 768, Short.MAX_VALUE)
-			    .addComponent(sharePanel, GroupLayout.Alignment.LEADING, 0, 
-			    	768, Short.MAX_VALUE)
-			)
-			.addContainerGap()
+		appFrameLayout.setVerticalGroup(
+			appFrameLayout.createSequentialGroup()
+			.addComponent(appPanelScrollPane, GroupLayout.PREFERRED_SIZE, 600, 
+				GroupLayout.PREFERRED_SIZE)
 		);
 	}
 	
--- src/app/gui/components/panels/SharePanel.java
+++ src/app/gui/components/panels/SharePanel.java
@@ -1,14 +1,48 @@
 package app.gui.components.panels;
 
 import javax.swing.BorderFactory;
+import javax.swing.GroupLayout;
+import javax.swing.JComponent;
 import javax.swing.JPanel;
+import javax.swing.LayoutStyle;
 
 public class SharePanel extends JPanel {
 
 	private static final long serialVersionUID = -7532452903844152513L;
+	
+	private JPanel uploadPanel;
+	private JPanel mailPanel;
 
 	public SharePanel() {
-		setSize(768, 133);
+		GroupLayout sharePanelLayout = new GroupLayout((JComponent) this);
+		setLayout(sharePanelLayout);
+		setSize(768, 491);
 		setBorder(BorderFactory.createTitledBorder("Upload and Share"));
+		
+		uploadPanel = new UploadPanel();
+		mailPanel = new MailPanel();
+		
+		setContent(sharePanelLayout);
+	}
+
+	private void setContent(GroupLayout sharePanelLayout) {
+		sharePanelLayout.setHorizontalGroup(
+			sharePanelLayout.createSequentialGroup()
+			.addContainerGap()
+			.addGroup(sharePanelLayout.createParallelGroup()
+			    .addComponent(uploadPanel, GroupLayout.Alignment.LEADING, 0, 
+			    		791, Short.MAX_VALUE)
+			    .addComponent(mailPanel, GroupLayout.Alignment.LEADING, 0, 
+			    		791, Short.MAX_VALUE))
+			.addContainerGap()
+		);
+		sharePanelLayout.setVerticalGroup(
+			sharePanelLayout.createSequentialGroup()
+			.addComponent(uploadPanel, GroupLayout.PREFERRED_SIZE, 70, 
+					GroupLayout.PREFERRED_SIZE)
+			.addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+			.addComponent(mailPanel, 0, 99, Short.MAX_VALUE)
+			.addContainerGap()
+		);
 	}
 }
